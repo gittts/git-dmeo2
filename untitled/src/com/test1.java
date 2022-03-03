b158c809776f69e2e16dbda335cb47022c991269
gittts <706512657@qq.com>
2022-03-03 21:50:32 +0800

    第二次

--- untitled/src/com/test1.java
+++ untitled/src/com/test1.java
@@ -1,4 +1,11 @@
 package com;
 
+import com.sun.javaws.IconUtil;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class test1 {
+    public static void main(String[] args){
+        System.out.println("hallo git");
+    }
 }
+
